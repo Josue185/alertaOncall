06595d1171b1899a22ca676c2a4d72c6bf81a933
Josué Dos Santos Domingues <92592495+Josue185@users.noreply.github.com>
2023-05-17 13:50:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Função para exibir uma notificação na área de trabalho
 function notify(message) {
   playSound().then(() => {
     if (!("Notification" in window)) {
@@ -25,84 +24,15 @@ async function playSound() {
   });
 }
 
-notify("Exemplo de notificação com som");
-  
-  
-  const lista = document.getElementById("lista");
-  const adicionar = document.getElementById("adicionar");
-  
-  adicionar.addEventListener("click", () => {
-    const hora = document.getElementById("hora").value;
-  
-    if (hora) {
-      const agora = new Date();
-      const [horas, minutos] = hora.split(":");
-      const data = new Date(agora.getFullYear(), agora.getMonth(), agora.getDate(), horas, minutos);
-  
-      const item = document.createElement("li");
-      item.innerHTML = `${hora} <button>Fechar</button>`;
-  
-      if (data < agora) {
-        item.classList.add("passado");
-      } else {
-        const tempo = data - agora;
-        setTimeout(() => {
-          notify(`É hora de colocar Oncall ${hora}.`);
-          item.classList.add("passado");
-        }, tempo);
-      }
-  
-      const fechar = item.querySelector("button");
-      fechar.addEventListener("click", () => {
-        item.remove();
-      });
-  
-      lista.appendChild(item);
-  
-      // Armazena a lista de horários no localStorage
-      const horarios = Array.from(lista.children).map((item) => item.textContent.trim().replace("Fechar", ""));
-      localStorage.setItem("horarios", JSON.stringify(horarios));
-  
-      document.getElementById("hora").value = "";
-    }
-  });
-  
-  // Recupera a lista de horários do localStorage
-  const horarios = JSON.parse(localStorage.getItem("horarios")) || [];
-  
-  // Cria a lista de horários na página com base na lista recuperada do localStorage
-  horarios.forEach((hora) => {
-    const agora = new Date();
-    const [horas, minutos] = hora.split(":");
-    const data = new Date(agora.getFullYear(), agora.getMonth(), agora.getDate(), horas, minutos);
-  
-    const item = document.createElement("li");
-    item.innerHTML = `${hora} <button>Fechar</button>`;
-  
-    if (data < agora) {
-      item.classList.add("passado");
-    } else {
-      const tempo = data - agora;
-      setTimeout(() => {
-        notify(`É hora de colocar Oncall ${hora}.`);
-        item.classList.add("passado");
-      }, tempo);
-    }
-  
-    const fechar = item.querySelector("button");
-    fechar.addEventListener("click", () => {
-      item.remove();
-  
-      // Armazena a lista de horários no localStorage
-      const horarios = Array.from(lista.children).map((item) => item.textContent.trim().replace("Fechar", ""));
-      localStorage.setItem("horarios", JSON.stringify(horarios));
-    });
-  
-    lista.appendChild(item);
-  });
-  
-  //função que chama o som de notificação
-  function playSound(){
-    const audio = new Audio("Alerta Oncall/IPHONE NOTIFICATION SOUND EFFECT (PINGDING).mp3")
-  }
-  
+// Código para testar o alerta e reprodução do som
+const horaProgramada = new Date("2023-05-18T12:00:00"); // Substitua pela hora desejada
+const agora = new Date();
+
+if (horaProgramada <= agora) {
+  notify("Hora de colocar Oncall");
+} else {
+  const tempoRestante = horaProgramada - agora;
+  setTimeout(() => {
+    notify("Hora de colocar Oncall");
+  }, tempoRestante);
+}
