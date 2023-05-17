e65eff0eb2b0b786eab7c52758027296b0d23fe8
Josué Dos Santos Domingues <92592495+Josue185@users.noreply.github.com>
2023-05-17 13:58:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,104 @@
-function notify(message) {
-  playSound().then(() => {
+// Função para exibir uma notificação na área de trabalho
+// Função para reproduzir o som
+function playSound() {
+    const audio = new Audio("/Alerta Oncall/IPHONE NOTIFICATION SOUND EFFECT (PINGDING).mp3");
+    audio.play().catch((error) => {
+      console.log("O som não pôde ser reproduzido:", error);
+    });
+  }
+  
+  function notify(message) {
     if (!("Notification" in window)) {
       console.log("Este navegador não suporta notificações de desktop");
     } else if (Notification.permission === "granted") {
       new Notification(message);
+      playSound();
     } else if (Notification.permission !== "denied") {
       Notification.requestPermission().then((permission) => {
         if (permission === "granted") {
           new Notification(message);
+          playSound();
         }
       });
     }
+  }
+  
+  
+  const lista = document.getElementById("lista");
+  const adicionar = document.getElementById("adicionar");
+  
+  adicionar.addEventListener("click", () => {
+    const hora = document.getElementById("hora").value;
+  
+    if (hora) {
+      const agora = new Date();
+      const [horas, minutos] = hora.split(":");
+      const data = new Date(agora.getFullYear(), agora.getMonth(), agora.getDate(), horas, minutos);
+  
+      const item = document.createElement("li");
+      item.innerHTML = `${hora} <button>Fechar</button>`;
+  
+      if (data < agora) {
+        item.classList.add("passado");
+      } else {
+        const tempo = data - agora;
+        setTimeout(() => {
+          notify(`É hora de colocar Oncall ${hora}.`);
+          item.classList.add("passado");
+        }, tempo);
+      }
+  
+      const fechar = item.querySelector("button");
+      fechar.addEventListener("click", () => {
+        item.remove();
+      });
+  
+      lista.appendChild(item);
+  
+      // Armazena a lista de horários no localStorage
+      const horarios = Array.from(lista.children).map((item) => item.textContent.trim().replace("Fechar", ""));
+      localStorage.setItem("horarios", JSON.stringify(horarios));
+  
+      document.getElementById("hora").value = "";
+    }
   });
-}
-
-// Função assíncrona para reproduzir o som
-async function playSound() {
-  return new Promise((resolve, reject) => {
-    const audio = new Audio("IPHONE NOTIFICATION SOUND EFFECT (PINGDING).mp3");
-    audio.onended = resolve;
-    audio.onerror = reject;
-    audio.play();
+  
+  // Recupera a lista de horários do localStorage
+  const horarios = JSON.parse(localStorage.getItem("horarios")) || [];
+  
+  // Cria a lista de horários na página com base na lista recuperada do localStorage
+  horarios.forEach((hora) => {
+    const agora = new Date();
+    const [horas, minutos] = hora.split(":");
+    const data = new Date(agora.getFullYear(), agora.getMonth(), agora.getDate(), horas, minutos);
+  
+    const item = document.createElement("li");
+    item.innerHTML = `${hora} <button>Fechar</button>`;
+  
+    if (data < agora) {
+      item.classList.add("passado");
+    } else {
+      const tempo = data - agora;
+      setTimeout(() => {
+        notify(`É hora de colocar Oncall ${hora}.`);
+        item.classList.add("passado");
+      }, tempo);
+    }
+  
+    const fechar = item.querySelector("button");
+    fechar.addEventListener("click", () => {
+      item.remove();
+  
+      // Armazena a lista de horários no localStorage
+      const horarios = Array.from(lista.children).map((item) => item.textContent.trim().replace("Fechar", ""));
+      localStorage.setItem("horarios", JSON.stringify(horarios));
+    });
+  
+    lista.appendChild(item);
   });
-}
-
-// Código para testar o alerta e reprodução do som
-const horaProgramada = new Date("2023-05-18T12:00:00"); // Substitua pela hora desejada
-const agora = new Date();
-
-if (horaProgramada <= agora) {
-  notify("Hora de colocar Oncall");
-} else {
-  const tempoRestante = horaProgramada - agora;
-  setTimeout(() => {
-    notify("Hora de colocar Oncall");
-  }, tempoRestante);
-}
+  
+  //função que chama o som de notificação
+  function playSound(){
+    const audio = new Audio("Alerta Oncall/IPHONE NOTIFICATION SOUND EFFECT (PINGDING).mp3")
+  }
+  
